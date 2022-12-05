62799b08a90ffb5d7524b1075eb38efbaec71976
delmashajan <delmashajan@gmail.com>
2022-12-05 22:36:17 +0530

    adminpage

--- src/main/java/com/example/libraryapp_backend/controller/LibraryController.java
+++ src/main/java/com/example/libraryapp_backend/controller/LibraryController.java
@@ -1,8 +1,14 @@
 package com.example.libraryapp_backend.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
 public class LibraryController {
+
+    @PostMapping("/")
+    public String Adminpage(){
+        return "admin page";
+    }
 }
